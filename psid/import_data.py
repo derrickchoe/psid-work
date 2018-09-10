6fd3706f93513cd5e7b939e23b658130db02a30f
Derrick Choe <Derrick.Choe@AEI.org>
2018-09-10 09:09:27 -0400

    fix subdirectories

--- psid/import_data.py
+++ psid/import_data.py
@@ -2,8 +2,9 @@ import os
 os.chdir("../")
 
 # Format Individual Data
+# NEED TO SET TO SUBFOLDERS
 path = "IndividualData/ind2015er/"
-folderpath = os.getcwd() +"\IndividualData"
+folderpath = os.getcwd() +"\IndividualData\ind2015er"
 readpath = path + "IND2015ER.do"
 writepath = path + "IND2015ERnew.do"
 with open(readpath, "r") as file:
@@ -12,11 +13,11 @@ with open(readpath, "r") as file:
 with open(writepath, "w") as file:
     file.write("cd " + folderpath)
     file.write(fix)
-    file.write("\nsave individual, replace;")
+    file.write("\nsave ..\individual, replace;")
 
 # Format Child History Data
 path = "ChildHistoryData/CAH85_15/"
-folderpath = os.getcwd() +"\ChildHistoryData"
+folderpath = os.getcwd() +"\ChildHistoryData\CAH85_15"
 readpath = path + "CAH85_15.do"
 writepath = path + "CAH85_15new.do"
 with open(readpath, "r") as file:
@@ -25,7 +26,7 @@ with open(readpath, "r") as file:
 with open(writepath, "w") as file:
     file.write("cd " + folderpath)
     file.write(fix)
-    file.write("\nsave childhistory, replace;")
+    file.write("\nsave ..\childhistory, replace;")
 
 
 # Format Family Data
@@ -73,3 +74,15 @@ master.write("do \"wlth`i'new.do\" \n")
 master.write("save \"../wlth`i'.dta\", replace \n")
 master.write("}")
 master.close()
+
+# master do file
+with open("format_data.do", "w") as file:
+    file.write("cd " + os.getcwd() + "\n")
+    # individual data
+    file.write("do IndividualData\ind2015er\IND2015ERnew.do" + "\n")
+    # child history data
+    file.write("do ChildHistoryData\CAH85_15\CAH85_15new.do" + "\n")
+    # family data
+    file.write("do FamilyData\\formatfam1.do" + "\n")
+    # wealth data
+    file.write("do WealthData\\formatwealth1.do")
